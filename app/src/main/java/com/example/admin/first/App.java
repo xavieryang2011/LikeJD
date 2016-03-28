a34a1a350f66cb34ff88e18a1914366714490043
yangxh <yangxh@chanjet.com>
2016-03-28 20:44:10 +0800

    0328

---
+++ app/src/main/java/com/example/admin/first/App.java
@@ -0,0 +1,16 @@
+package com.example.admin.first;
+
+import android.app.Application;
+
+/**
+ * Created by yangxh on 2016/3/28.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        //init
+    }
+}
